bce7f72333c2d86bc5f378d27dfd2c6125a462fe
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-10 11:36:53 +0300

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const names = ['Sebi', 'Andrei', 'Raul', 'Cosmin'];
+const grade = 'A';
 
-let i = 0;
-while(i < 5 ){
-    console.log(names)
-    i++;
-}
-
-while(i < names.length){
-    console.log(i);
-    i++
+if(grade == 'A') {
+    console.log("Your grade is A");
+} else if(grade == 'B') {
+    console.log("Your grade is B");
+} else if (grade == 'C') {
+    console.log("Your grade is C");
+} else if (grade == 'D') {
+    console.log("Your grade is D");
+} else if (grade == 'F') {
+    console.log("Your grade is F");
 }
