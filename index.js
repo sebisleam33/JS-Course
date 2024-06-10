39f40d0601864f63ed60179ed69019da8b8ce4f7
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-10 13:31:53 +0300

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // let element = document.querySelector('b#this_is_id')
-let element = document.querySelector('div').querySelector('.para')
+// let element = document.querySelector('div').querySelector('.para')
+let elements = document.querySelectorAll('p')
 
-console.log('element: ', element)
+console.log('element: ', elements)
+
+elements.forEach(element => {
+    console.log(element)
+})
