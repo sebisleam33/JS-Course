6f341461581e8488cac84382368448be39139912
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-10 11:40:10 +0300

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const grade = 'A';
 
-if(grade == 'A') {
-    console.log("Your grade is A");
-} else if(grade == 'B') {
-    console.log("Your grade is B");
-} else if (grade == 'C') {
-    console.log("Your grade is C");
-} else if (grade == 'D') {
-    console.log("Your grade is D");
-} else if (grade == 'F') {
-    console.log("Your grade is F");
+switch(grade) {
+    case 'A':
+        console.log('Great!');
+        break;
+    case 'B':
+        console.log('Good');
+        break;
+    case 'C':
+        console.log('Average');
+        break;
+    case 'D':
+        console.log('Bad');
+        break;
+    case 'F':
+        console.log('Fail');
+        break;
+    default:
+        console.log('Invalid grade');
 }
