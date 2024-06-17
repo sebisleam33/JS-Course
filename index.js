a52a41ec7367d4fabdcec76e86a456fe6a73c619
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-17 09:41:31 +0300

    Updates

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-let numbers = [0, 10, 5, 27, 3, 10, 12]
+let names = ["sebi", "dragos", "ana", "ama"]
 
-numbers.sort();
-console.log(numbers);
+let students = names.filter(name => name != "sebi")
+                        .map(name => name.toUpperCase())
+                        .sort()
+                        .reverse()
 
-let products = [
-    {name:"shoe", price:15},
-    {name:"shirt", price:3},
-    {name:"pants", price:10},
-    {name:"bag", price:6}
-]
-
-products.sort((a,b) => {
-    if(a.price > b.price) {
-        return 1;
-    } else if (a.price < b.price) {
-        return -1;
-    } else {
-        return 0;
-    }
-});
-
-console.log(products);
+console.log(students)
