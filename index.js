1e85e907269ee4097c5efe8237e94fb2667f7ea4
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-17 09:17:32 +0300

    Updates

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// let elements = [0, 20, 15, 35, 23, 66, 44]
+let numbers = [0, 10, 5, 27, 3, 10, 12]
 
-// let even_numbers = elements.filter(number => {
-//     return number % 2 == 0;
-// });
+let sq_numbers = numbers.map(number => number * number);
 
-let numbers = [
-    {numbers: 12, type: "even"},
-    {numbers: 6, type: "even"},
-    {numbers: 0, type: "even"},
-    {numbers: 2, type: "even"},
-    {numbers: 5, type: "odd"},
-    {numbers: 27, type: "odd"},
-    {numbers: 59, type: "odd"}
+// console.log(sq_numbers, numbers);
+
+let numbers_pair = [
+    {number: 12, type: "square"},
+    {number: 0, type: "cube"},
+    {number: 6, type: "square"},
+    {number: 22, type: "square"},
+    {number: 59, type: "cube"}
 ]
 
-let even_numbers = numbers.filter(number => number.type == "even");
+let processed_numbers = numbers_pair.map(number => {
+    if(number.type == "square") {
+        return number.number * number.number;
+    } else {
+        return number.number * number.number *number.number;
+    }
+});
 
-console.log(elements);
+console.log(processed_numbers, numbers_pair);
